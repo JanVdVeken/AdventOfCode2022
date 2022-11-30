4fbe530233a42f9a9b91581675f7409e50f48d4c
JanVdV <jan.vanderveken@external.atlascopco.com>
2022-11-30 14:52:13 +0100

    Added usersecrets

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,10 +1,20 @@
-﻿using Common.Services;
+﻿using Common.Clients;
+using Common.Services;
+using Microsoft.Extensions.Configuration;
 
 try
 {
-    IInputService inputService = await InputService.CreateInputServiceAsync(2022);
+    var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .AddUserSecrets<AoCConfig>()
+        .Build();
+    var section = config.GetSection(nameof(AoCConfig));
+    var aocClient = section.Get<AoCConfig>();
+    IInputService inputService = await InputService.CreateInputServiceAsync(2021, aocClient);
 }
-catch(Exception exception)
+catch (Exception exception)
 {
     Console.WriteLine($"The following exception occured: {exception.Message}");
 }
+    
