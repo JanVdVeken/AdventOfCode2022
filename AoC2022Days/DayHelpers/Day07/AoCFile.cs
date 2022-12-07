76e0863aad57618144f4ca79d66105efd0e0e2fc
JanVdV <jan.vanderveken@external.atlascopco.com>
2022-12-07 12:28:05 +0100

    Cleanup

--- AoC2022Days/DayHelpers/Day07/AoCFile.cs
+++ AoC2022Days/DayHelpers/Day07/AoCFile.cs
@@ -4,22 +4,22 @@ namespace AoC2022Days.DayHelpers.Day07;
 
 public class AoCFile
 {
-	private readonly string _name;
-	private readonly int _size; //does not matter! Motion of the ocean!
-	public int Size => _size;
-	public string Name => _name;
-	public AoCFile(string input)
-	{
-		var temp = input.Trim().Split(" ");
-       	 	_size = int.Parse(temp[0].Trim());
-       		_name = temp[1].Trim();
-   	 }   
+    private readonly string _name;
+    private readonly int _size; //does not matter! Motion of the ocean!
+    public int Size => _size;
+    public string Name => _name;
+    public AoCFile(string input)
+    {
+        var temp = input.Trim().Split(" ");
+        _size = int.Parse(temp[0].Trim());
+        _name = temp[1].Trim();
+    }
 
-	public string ToString(int indentations)
-	{
-		StringBuilder sb = new StringBuilder();
+    public string ToString(int indentations)
+    {
+        StringBuilder sb = new StringBuilder();
         sb.Append(new string(' ', 2 * indentations));
         sb.Append($"- {_name} (file, size={_size})");
         return sb.ToString();
-	}
+    }
 }
