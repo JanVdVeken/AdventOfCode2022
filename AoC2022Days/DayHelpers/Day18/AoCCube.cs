8c554416ef5cd0bacda720a2853f692a36a88e6d
JanVdVeken <vekie1@hotmail.com>
2022-12-18 13:01:59 +0100

    Solved D18P1 and started D18P2

---
+++ AoC2022Days/DayHelpers/Day18/AoCCube.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC2022Days.DayHelpers.Day18;
+
+public class AoCCube
+{
+    public int X;
+    public int Y;
+    public int Z;
+
+    public AoCCube(string input)
+    {
+        var temp = input.Split(",");
+        X = int.Parse(temp[0]);
+        Y = int.Parse(temp[1]);
+        Z = int.Parse(temp[2]);
+    }
+    public AoCCube(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public int CountFreeSides(List<AoCCube> cubes)
+    {
+        return 6 - cubes.Count(c => (c.X == X && c.Y == Y && (c.Z == Z + 1 || c.Z == Z - 1))
+                                || (c.X == X && c.Z == Z && (c.Y == Y - 1 || c.Y == Y + 1))
+                                || (c.Y == Y && c.Z == Z && (c.X == X - 1 || c.X == X + 1)));
+    }
+
+    public bool AreEqual(AoCCube other)
+    {
+        return X == other.X && Y == other.Y && Z == other.Z;
+    }
+
+    public bool HasNeighbour(List<AoCCube> cubes)
+    {
+        return cubes.Any(c => (c.X == X && c.Y == Y && (c.Z == Z + 1 || c.Z == Z - 1))
+                              || (c.X == X && c.Z == Z && (c.Y == Y - 1 || c.Y == Y + 1))
+                              || (c.Y == Y && c.Z == Z && (c.X == X - 1 || c.X == X + 1)));
+    }
+}
---
+++ Aoc2022Days.Test/Day18/Day18Test.cs
@@ -0,0 +1,47 @@
+﻿using Common.Services;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Aoc2022Days.Test.Day18;
+
+[TestFixture]
+public class Day18Test
+{
+    [Test]
+    public void Puzzle1_WithTestInput_ShouldReturn15()
+    {
+        var answerService = Substitute.For<IAnswerService>();
+        var inputService = Substitute.For<IInputService>();
+        var day = new AoC2022Days.Days.Day18(inputService, answerService, 18, "Boiling Boulders");
+
+        var result = day.Puzzle1(inputList);
+
+        Assert.AreEqual("64", result);
+    }
+
+    [Test]
+    public void Puzzle2_WithTestInput_ShouldReturn15()
+    {
+        var answerService = Substitute.For<IAnswerService>();
+        var inputService = Substitute.For<IInputService>();
+        var day = new AoC2022Days.Days.Day18(inputService, answerService, 18, "Boiling Boulders");
+
+        var result = day.Puzzle2(inputList);
+
+        Assert.AreEqual("58", result);
+    }
+
+    private List<string> inputList = @"2,2,2
+                                        1,2,2
+                                        3,2,2
+                                        2,1,2
+                                        2,3,2
+                                        2,2,1
+                                        2,2,3
+                                        2,2,4
+                                        2,2,6
+                                        1,2,5
+                                        3,2,5
+                                        2,1,5
+                                        2,3,5".Split("\r\n").ToList();
+}
